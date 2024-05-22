a12be21eb221edeb919bd6710a28f4d61fb1f8c0
BrianFaviansa <brianfpd31@gmail.com>
2024-05-22 16:13:33 +0700

    1.3 prototype on progress

--- 1.3-prototype/script.js
+++ 1.3-prototype/script.js
@@ -23,34 +23,34 @@
 // 2. function declaration
 // 
 
-const methodMahasiswa = {
-    makan: function (porsi) {
-        this.energi += porsi;
-        console.log(`Halo ${this.nama}, selamat makan!`);
-    },
+// const methodMahasiswa = {
+//     makan: function (porsi) {
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, selamat makan!`);
+//     },
 
-    main: function (jam) {
-        this.energi -= jam;
-        console.log(`Halo ${this.nama}, selamat bermain!`);
-    },
+//     main: function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain!`);
+//     },
 
-    tidur: function (jam) {
-        this.energi += jam * 2;
-        console.log(`Halo ${this.nama}, selamat tidur!`);
-    }
-}
+//     tidur: function (jam) {
+//         this.energi += jam * 2;
+//         console.log(`Halo ${this.nama}, selamat tidur!`);
+//     }
+// }
 
-function Mahasiswa(nama, energi) {
-    let mahasiswa = Object.create(methodMahasiswa);
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
-    // 3 method dibawah sudah tidak perlu karena bisa menggunakan object.create()
-    // mahasiswa.makan = methodMahasiswa.makan;
-    // mahasiswa.main = methodMahasiswa.main;
-    // mahasiswa.tidur = methodMahasiswa.tidur;
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
+//     // 3 method dibawah sudah tidak perlu karena bisa menggunakan object.create()
+//     // mahasiswa.makan = methodMahasiswa.makan;
+//     // mahasiswa.main = methodMahasiswa.main;
+//     // mahasiswa.tidur = methodMahasiswa.tidur;
 
-    return mahasiswa;
-}
+//     return mahasiswa;
+// }
 
 // let hibou = Mahasiswa('Hibou', 10);
 // let furby = Mahasiswa('Furby', 20);
@@ -74,6 +74,48 @@ function Mahasiswa(nama, energi) {
 // let hibou = new Mahasiswa('Hibou', 10);
 // let furby = new Mahasiswa('Furby', 20);
 
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+// }
 
+// Mahasiswa.prototype.makan = function(porsi) {
+//     this.energi += porsi;
+//     return `Halo ${this.nama}, selamat makan!`;
 
-// 4. object.create()
+// }
+// Mahasiswa.prototype.main = function(jam) {
+//     this.energi -= jam;
+//     return `Halo ${this.nama}, selamat bermain!`;
+// }
+
+// Mahasiswa.prototype.tidur = function(jam) {
+//     this.energi += jam * 2;
+//     return `Halo ${this.nama}, selamat tidur!`;
+// }
+
+// let hibou = new Mahasiswa('Hibou', 10);
+
+// VERSI CLASS
+
+class Mahasiswa {
+    constructor(nama, energi) {
+        this.nama = nama;
+        this.energi = energi;
+    }
+
+    makan(porsi) {
+        this.energi += porsi;
+        return `Halo ${this.nama}, selamat makan!`;
+    }
+
+    main(jam) {
+        this.energi -= jam;
+        return `Halo ${this.nama}, selamat bermain!`;
+    }
+
+    tidur(jam) {
+        this.energi += jam * 2;
+        return `Halo ${this.nama}, selamat tidur!`;
+    }
+}
