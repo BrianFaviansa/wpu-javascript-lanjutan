5fd2aa383e0c0d81418121009e1c62b15caff2b9
BrianFaviansa <brianfpd31@gmail.com>
2024-05-24 18:21:28 +0700

    4.1 higher order function done

---
+++ 4.2-filter, map, dan reduce/script.js
@@ -0,0 +1,12 @@
+//* javascript memperlakukan function sebagai first-class object, yang berarti function bisa disimpan dalam variabel, bisa dijadikan sebagai parameter, dan bisa dijadikan sebagai return value.
+
+//* function yang disimpan sebagai argumen disebut callback function.
+//* function yang memiliki function sebagai argumen disebut higher order function.
+
+//  alasan menggunakan higher order function:
+// abstraksi: kita bisa menyembunyikan detail dari sebuah function dan hanya mengekspose apa yang dibutuhkan oleh user.
+
+// mendekatkan ke functional programming
+
+// contoh higher order function
+// Array.prototype.map(), Array.prototype.filter(), Array.prototype.reduce()
