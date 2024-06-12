19639727d4f724fdfb6dfecbe297789459313517
BrianFaviansa <brianfpd31@gmail.com>
2024-06-12 17:51:59 +0700

    5.2 latihan template literals done

--- 5.2 latihan template literals/script.js
+++ 5.2 latihan template literals/script.js
@@ -16,16 +16,88 @@ const x = 10;
 console.log(`${(x & 2 == 0) ? 'genap' : 'ganjil'}`);
 
 // html frafments
+// const mhs = {
+//     nama: 'Hibou',
+//     umur: 20,
+//     nrp: '12345678',
+//     email: 'hibou@gmail.com'
+// };
+
+// const el = `<div class="mhs">
+// <h2>${mhs.nama}</h2>
+// <span class="nrp">${mhs.nrp}</span>
+// </div>`;
+
+// console.log(el);
+
+// looping
+// const mhs = [
+//     {
+//         nama: 'Hibou',
+//         email: 'hibou@gmail.com'
+//     },
+//     {
+//         nama: 'Bebou',
+//         email: 'bebou@gmail.com'
+//     },
+//     {
+//         nama: 'Furby',
+//         email: 'furby@gmail.com'
+//     },
+// ];
+
+// const el = `<div class="mhs">
+// ${mhs.map(m => `<ul>
+//     <li>${m.nama}</li>
+//     <li>${m.email}</li>
+//     </ul>`).join('')}
+// </div>`
+
+
+// document.body.innerHTML = el;
+
+
+// conditionals
+// ternary
+// const lagu = {
+//     judul: 'Tetap Dalam Jiwa',
+//     penyanyi: 'Isyana Sarasvati',
+//     feat: 'Rayi Putra'
+// }
+
+// const el = `<div class="lagu">
+//     <ul>
+//     <li>${lagu.penyanyi}</li>
+//     <li>${lagu.judul}  ${lagu.feat ? `(feat. ${lagu.feat})` : ''}</li>
+//     </ul>
+//     </div>`;
+
+// 4. nested html fragments
+
 const mhs = {
     nama: 'Hibou',
-    umur: 20,
-    nrp: '12345678',
-    email: 'hibou@gmail.com'
+    semester: 5,
+    mataKuliah: [
+        'Rekayasa Web',
+        'Analisis dan Perancangan Sistem Informasi',
+        'Pemrograman Sistem Interaktif',
+        'Perancangan Sistem Berorientasi Object'
+    ]
 };
 
-const el = `<div class="mhs">
-<h2>${mhs.nama}</h2>
-<span class="nrp">${mhs.nrp}</span>
-</div>`;
+function cetakMataKuliah(mataKuliah) {
+    return `
+    <ol>
+    ${mataKuliah.map(mk => `<li>${mk}</li>`).join('')}
+    </ol>
+    `;
+}
 
-console.log(el);
+const el = `<div class="mhs">
+    <h2>${mhs.nama}</h2>
+    <span class="semester">Semester : ${mhs.semester}</span>
+    <h4>Mata Kuliah:</h4>
+        ${cetakMataKuliah(mhs.mataKuliah)}
+</div>`
+
+document.body.innerHTML = el;
