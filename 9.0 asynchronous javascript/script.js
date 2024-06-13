9390c063e917b9e617aa21eccf8d8ac467b18ee4
BrianFaviansa <brianfpd31@gmail.com>
2024-06-13 09:38:28 +0700

    9.0 asnyc js done

---
+++ 9.0 asynchronous javascript/script.js
@@ -0,0 +1,25 @@
+// ! asynchronous javascript
+
+// * javscript is a single-threaded, non-blocking, asynchronous, and concurrent language
+
+// * thread => urutan eksekusi program yang bisa berjalan secara bebas / independent satu sama lain
+// * non blocking  => bisa menjalankan proses lain tanpa harus menunggu proses yang lain selesai
+// * asynchronous => proses yang berjalan tidak selalu berurutan
+// * asynchrnous + single-threaded => concurrency
+// * asynchronous + multi-threaded => parallelism
+
+// * single threaded vs multi threaded => lingkungan eksekusi task
+// * blocking vs non-blocking => cara program menjalankan proses (IO related
+// * synchronous vs asynchronous => cara program menjalankan proses (HTTP request related)
+// * concurrent vs parallel => lingkungan eksekusi task (cpu related)
+
+// * heap => memory allocation
+// * stack => function call (execution context)
+
+// * function dapat menerima parameter berupa callback function
+
+// * tujuannya jangan sampai block event loop 
+
+
+
+
